95b816e9d55540ca0a71735b1f93f514ce7a0b40
leovsch <leo.25.1996@gmail.com>
2024-05-15 16:56:04 +0200

    changed port number such that it is supported by kubernetes

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ COPY requirements.txt ./
 
 RUN pip install -r requirements.txt
 
+EXPOSE 30000
+
 ENTRYPOINT python3 src/main.py
--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import helics as h
 MS_TO_BROKER_DISCONNECT = 600000
 
 def main():
-    broker = h.helicsCreateBroker("zmq", "helics_broker", "-f 2 --loglevel=debug --brokerport 23404")
+    broker = h.helicsCreateBroker("zmq", "helics_broker", "-f 2 --loglevel=debug --ipv4 --timeout='60s' --port=30000")
     broker.wait_for_disconnect(MS_TO_BROKER_DISCONNECT)
-    h.createval
+
 
 if __name__ == "__main__":
     exit(main())
