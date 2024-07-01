c2973cf164306285ba3539e0cb55013e4333bf68
leovsch <leo.25.1996@gmail.com>
2024-07-01 10:32:46 +0200

    get amount of federates from environment

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ MS_TO_BROKER_DISCONNECT = 600000
 def main():
     amount_of_federates = os.getenv("AMOUNT_OF_FEDERATES", "2")
     broker_port = os.getenv("HELICS_BROKER_PORT", "30000")
-    amount_of_esdl_message_federates = int(os.getenv("AMOUNT_OF_ESDL_MESSAGE_FEDERATES", "2")) + 1
+    amount_of_esdl_message_federates = int(os.getenv("AMOUNT_OF_ESDL_MESSAGE_FEDERATES", "2"))
 
     broker = h.helicsCreateBroker("zmq", "helics_broker_esdl", f"-f {amount_of_esdl_message_federates} --loglevel=debug --ipv4 --timeout='60s' --brokerport={broker_port} --port={broker_port}")
     broker.wait_for_disconnect(MS_TO_BROKER_DISCONNECT)
