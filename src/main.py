ec8d4ad362d2c922d9e45f8afa4c4f23990d05dd
leovsch <leo.25.1996@gmail.com>
2024-11-20 10:24:46 +0100

    Update main.py
    set global time property for esdl message federate

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def main():
     broker_port = os.getenv("HELICS_BROKER_PORT", "30000")
     amount_of_esdl_message_federates = int(os.getenv("AMOUNT_OF_ESDL_MESSAGE_FEDERATES", "2"))
 
-    broker = h.helicsCreateBroker("zmq", "helics_broker_esdl", f"-f {amount_of_esdl_message_federates} --loglevel=debug --ipv4 --timeout='60s' --brokerport={broker_port} --port={broker_port}")
+    broker = h.helicsCreateBroker("zmq", "helics_broker_esdl", f"-f {amount_of_esdl_message_federates} --loglevel=debug --ipv4 --timeout='60s' --brokerport={broker_port} --port={broker_port} --globaltime")
     broker.wait_for_disconnect(MS_TO_BROKER_DISCONNECT)
 
     LOGGER.info("Start broker with global time")
